9e80c631d5330fc00cdd6c4264347176aa59ccf5
Tonjr <wwffjr@gmail.com>
2026-06-09 11:24:58 -0300

    Exercício 091 - Jogo de Dados e Ordenação
    Implementação de sorteio aleatório, uso de time.sleep para delay e ordenação de dicionários via operator.itemgetter.

--- MUNDO 3 PYTHON/Dicionarios/ex.091(reforco_ordenacao).py
+++ MUNDO 3 PYTHON/Dicionarios/ex.091(reforco_ordenacao).py
@@ -1,1 +1,13 @@
-print("Olá, Tonjr! Tudo funcionando.")
+from operator import itemgetter
+sistema_vendas = {
+    'Tonjr':15800,
+    'Isaac':13000,
+    'Fatima':90000,
+    'Carol':88899
+}
+ordenacoes = []
+for k, v in sistema_vendas.items():
+    print(f'{k} tirou {v}')
+ordenacoes = sorted(sistema_vendas.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ordenacoes):
+    print(f'{i+1}º - {v[0]} - {v[1]}')
